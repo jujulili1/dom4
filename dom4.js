12a4fba9a33e7e4f86bdd3f773f8986b4265f044
jujulili1 <tyulianto0795@gmail.com>
2020-06-16 16:31:16 +0700

    first commit

---
+++ dom4.js
@@ -0,0 +1,25 @@
+const title = document.getElementById('judul');
+title.innerHTML = `<h1>Resep Ayam Geprek</h1>`;
+
+const isi = document.getElementById('resep');
+isi.innerHTML = `
+<p>
+    <h1>Bahan-bahan</h1>
+    <ul>
+        <li> 2 Potong ayam </li>  
+        <li> 1 butir telur </li>  
+        <li> 5 sendok tepung terigu </li>  
+        <li> 3 sendok tepung maizena </li>  
+        <li> 3 siung bawang putih </li>  
+        <li> Secukupnya merica </li>  
+        <li> secukupnya garam </li>  
+        <li> Secukupnya minyak goreng </li>  
+        <li> 1 Batang serai </li>  
+    </ul>
+</p>
+`
+
+const images = document.getElementById('photo');
+images.innerHTML = `
+<img src="https://img-global.cpcdn.com/recipes/cfe37f82ce72ec3c/751x532cq70/ayam-geprek-pedas-ala-bensu-foto-resep-utama.jpg">
+`
